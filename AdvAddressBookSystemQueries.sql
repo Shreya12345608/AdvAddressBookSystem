4d28f3431a37814e144a64c93dfe3a6209ce2af4
Shreya Malviya <malviyashreya26@gmail.com>
2021-06-30 19:46:17 +0530

    [Shreya] Add:  Insert Data in database

--- AdvAddressBookSystemQueries.sql
+++ AdvAddressBookSystemQueries.sql
@@ -13,3 +13,9 @@ phoneNumber bigint,
 email varchar(100)
 );
 select * from AddressBook;
+
+
+insert into AddressBook values
+('Shreya','Malviya','Ngp','Nagpur','MH',440022,8596859685,'malviyashreya26@gmail.com'),
+('Prajakta','Bramhe','Mumbai123','Mumbai','Maharashtra',550022,78577964412,'prajaktab@gmail.com')
+
