314d3855dff25b9f7a509b60a45db2779a8f740a
Shreya Malviya <malviyashreya26@gmail.com>
2021-06-30 19:33:09 +0530

    [Shreya] Add:  Create Table

--- AdvAddressBookSystemQueries.sql
+++ AdvAddressBookSystemQueries.sql
@@ -1,1 +1,15 @@
 create database AddressBookServiceDB
+
+use AddressBookServiceDB
+create table AddressBook
+(
+firstName varchar(25),
+lastName varchar(25),
+address varchar(200),
+city varchar(25),
+state varchar(25),
+zip int,
+phoneNumber bigint,
+email varchar(100)
+);
+select * from AddressBook;
